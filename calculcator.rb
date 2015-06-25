1719fd2d5098ca9672591b2128782f9841ab50cd
1MrSelect <mrjameraqui@gmail.com>
2015-06-25 19:36:27 +0000

    FIRST CALCULATRON COMMIT

---
+++ calculcator.rb
@@ -0,0 +1,35 @@
+
+def method_add(num1, num2)
+  a = num1 + num2 
+  puts a
+  puts "(R)epeat, (C)hange operator, (E)xit"
+  b = gets.chomp
+  if b == "R" 
+    method_add
+#   elsif b == "C"
+#     calc
+  elsif b == "E"
+    exit
+end
+end
+method_add("1", "2")
+
+
+
+
+# puts "+,-,*,/"
+# operator = gets.chomp
+# if operator == "+"
+#   puts method_add
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
