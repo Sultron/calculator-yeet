e85e9a8c41a28fd3e07a76838ca1877eccb09d68
Sultron <firekicks@icloud.com>
2016-11-01 00:12:57 -0700

    Subtraction

--- calculcator.rb
+++ calculcator.rb
@@ -14,6 +14,21 @@ end
 end
 method_add("1", "2")
 
+def method_subtract(num1, num2)
+  a = num1 - num2 
+  puts a
+  puts "(R)epeat, (C)hange operator, (E)xit"
+  b = gets.chomp
+  if b == "R" 
+    method_add
+#   elsif b == "C"
+#     calc
+  elsif b == "E"
+    exit
+end
+end
+method_subtract("1", "2")
+
 
 
 
